d15a2ed08a1d31100b0a630bc93be5a0ec0009eb
Devadeth shaji <devadethofficial@gmail.com>
2025-05-14 13:52:22 +0530

    Making it advanced by learning TextBlob library

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
+
+from textblob import TextBlob
+# This program will ask the user how they are feeling and play a song based on their mood.
+
+
+
 mood = input("How are you feeling today? ").lower()
+blob_variable = TextBlob(mood).correct()
+# Check the sentiment of the mood
+sentiment_variable = blob_variable.sentiment.polarity
+# Print the sentiment   
 
+if sentiment_variable > 0.5:
+    print("🎵 Ith Pwoli Moood!!!")
+    
+elif  sentiment_variable < -0.5:
+    
+    print("🎵 Ith Shokappaattu ")
 
+# elif "angry" in mood:
+#     print("🎵 Playing: 'Numb' by Linkin Park'")
 
-if "happy" in mood:
-    print("🎵 Playing: 'Happy' by Pharrell Williams")
-
-elif "sad" in mood:
-    print("🎵 Playing: 'Fix You' by Coldplay")
-
-elif "angry" in mood:
-    print("🎵 Playing: 'Numb' by Linkin Park'")
-
-elif "love" in mood:
-    print("🎵 Playing: 'Perfect' by Ed Sheeran'")
+# elif "love" in mood:
+#     print("🎵 Playing: 'Perfect' by Ed Sheeran'")
 
 else:
-    print("🎵 Playing: 'Let It Be' by The Beatles'")    
+    print("🎵 Playing: Ith Edk Moood ")    
 
+
+print(sentiment_variable)
 
     
