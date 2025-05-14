af1acccb2dbcb7e92772f8cfa1703101680d8bb1
Devadeth shaji <devadethofficial@gmail.com>
2025-05-14 09:35:31 +0530

    first commit mood sonf recommender

---
+++ main.py
@@ -0,0 +1,21 @@
+mood = input("How are you feeling today? ").lower()
+
+
+
+if "happy" in mood:
+    print("🎵 Playing: 'Happy' by Pharrell Williams")
+
+elif "sad" in mood:
+    print("🎵 Playing: 'Fix You' by Coldplay")
+
+elif "angry" in mood:
+    print("🎵 Playing: 'Numb' by Linkin Park'")
+
+elif "love" in mood:
+    print("🎵 Playing: 'Perfect' by Ed Sheeran'")
+
+else:
+    print("🎵 Playing: 'Let It Be' by The Beatles'")    
+
+
+    
